0493b951a61f3ca3483ef61304e36009c872559a
Siddharth230 <siddharthlokhande04@gmail.com>
2024-11-07 09:19:06 +0530

    14e

--- Lesson 14/scripts/amazon.js
+++ Lesson 14/scripts/amazon.js
@@ -1,6 +1,7 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { updateCartQuantity } from "./checkout.js";
 
 let productsHTML = "";
 
@@ -62,11 +63,7 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }
