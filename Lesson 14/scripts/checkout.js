6c232d0da89f3fe16802e577ec0acd00619fc589
Siddharth230 <siddharthlokhande04@gmail.com>
2024-11-07 15:20:05 +0530

    14k

--- Lesson 14/scripts/checkout.js
+++ Lesson 14/scripts/checkout.js
@@ -43,7 +43,9 @@ cart.forEach((cartItem) => {
             }">
               Update
             </span>
-            <input class="quantity-input">
+            <input class="quantity-input js-quantity-input-${
+              matchingProduct.id
+            }">
             <span class="save-quantity-link link-primary js-save-link" data-product-id="${
               matchingProduct.id
             }">Save</span>
@@ -145,5 +147,10 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
       `.js-cart-item-container-${productId}`
     );
     container.classList.remove("is-editing-quantity");
+
+    const quantityInput = document.querySelector(
+      `.js-quantity-input-${productId}`
+    );
+    const newQuantity = Number(quantityInput.value);
   });
 });
