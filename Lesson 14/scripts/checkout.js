17634f67204ac2b1a64cd9e0685f85e72e31d222
Siddharth230 <siddharthlokhande04@gmail.com>
2024-11-07 09:11:34 +0530

    14a-14d

--- Lesson 14/scripts/checkout.js
+++ Lesson 14/scripts/checkout.js
@@ -105,5 +105,21 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     );
 
     container.remove();
+
+    updateCartQuantity();
   });
 });
+
+export function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(
+    ".js-return-to-home-link"
+  ).innerHTML = `${cartQuantity} items`;
+}
+
+updateCartQuantity();
